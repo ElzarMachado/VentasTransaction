90c1339a1e1a27cde73b9e2038fd264b4c092c99
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2023-03-18 14:07:28 -0700

    se quito error

--- AccesoDatos/VentaDetalle.cs
+++ AccesoDatos/VentaDetalle.cs
@@ -14,7 +14,7 @@ namespace AccesoDatos
         public decimal Cantidad { get; set; }
         public string Descripcion { get; set; }
         public decimal PrecioUnitario { get; set; }
-        public decimal Importe { get; set; };
+        public decimal Importe { get; set; }
 
     }
 }
